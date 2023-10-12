42fde7702b7df0833656e563b253e0ea2730f73b
Hishamk99 <hishamkamel370@gmail.com>
2023-10-12 16:35:16 +0200

    status code

--- lib/services/categories_service.dart
+++ lib/services/categories_service.dart
@@ -2,18 +2,22 @@ import 'dart:convert';
 import 'package:store_app/models/product_model.dart';
 import 'package:http/http.dart' as http;
 
-class CategoriesService
-{
-  Future<List<ProductModel>> getCategoriesProduct({required String categoryName}) async
-  {
-    http.Response response =
-        await http.get(Uri.parse('https://fakestoreapi.com/products/category/$categoryName'));
+class CategoriesService {
+  Future<List<ProductModel>> getCategoriesProduct(
+      {required String categoryName}) async {
+    http.Response response = await http.get(
+        Uri.parse('https://fakestoreapi.com/products/category/$categoryName'));
 
-   List<dynamic> data = jsonDecode(response.body);
-    List<ProductModel> productList = [];
-    for (int i = 0; i < data.length; i++) {
-      productList.add(ProductModel.fromJson(data[i]));
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      List<ProductModel> productList = [];
+      for (int i = 0; i < data.length; i++) {
+        productList.add(ProductModel.fromJson(data[i]));
+      }
+      return productList;
+    } else {
+      throw Exception(
+          'Theres a problem with status code ${response.statusCode}');
     }
-    return productList;     
   }
 }
--- lib/services/get_all_product_service.dart
+++ lib/services/get_all_product_service.dart
@@ -7,11 +7,15 @@ class AllProductService {
     http.Response response =
         await http.get(Uri.parse('https://fakestoreapi.com/products'));
 
-    List<dynamic> data = jsonDecode(response.body);
-    List<ProductModel> productList = [];
-    for (int i = 0; i < data.length; i++) {
-      productList.add(ProductModel.fromJson(data[i]));
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      List<ProductModel> productList = [];
+      for (int i = 0; i < data.length; i++) {
+        productList.add(ProductModel.fromJson(data[i]));
+      }
+      return productList;
+    } else{
+      throw Exception('Theres a problem with status code ${response.statusCode}');
     }
-    return productList;
   }
 }
