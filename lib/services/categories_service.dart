0abc97a959a9ff235935b74ffabd3eeee06e161e
Hishamk99 <hishamkamel370@gmail.com>
2023-10-12 17:54:01 +0200

    refactor code

--- lib/services/categories_service.dart
+++ lib/services/categories_service.dart
@@ -1,23 +1,18 @@
-import 'dart:convert';
+import 'package:store_app/helper/api.dart';
 import 'package:store_app/models/product_model.dart';
-import 'package:http/http.dart' as http;
 
 class CategoriesService {
-  Future<List<ProductModel>> getCategoriesProduct(
+  Future<List<dynamic>> getCategoriesProduct(
       {required String categoryName}) async {
-    http.Response response = await http.get(
-        Uri.parse('https://fakestoreapi.com/products/category/$categoryName'));
 
-    if (response.statusCode == 200) {
-      List<dynamic> data = jsonDecode(response.body);
+    List<dynamic> data  =
+        await Api().get(url: 'https://fakestoreapi.com/products/category/$categoryName');    
+
+  
       List<ProductModel> productList = [];
       for (int i = 0; i < data.length; i++) {
         productList.add(ProductModel.fromJson(data[i]));
       }
       return productList;
-    } else {
-      throw Exception(
-          'Theres a problem with status code ${response.statusCode}');
-    }
   }
 }
