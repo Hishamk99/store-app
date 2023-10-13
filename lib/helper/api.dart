dd371e87d7f192391d594b2a9af39d686e9552ed
Hishamk99 <hishamkamel370@gmail.com>
2023-10-13 14:00:40 +0200

    post method

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
@@ -12,4 +12,33 @@ class Api {
           'Theres a problem with status code ${response.statusCode}');
     }
   }
+  Future<dynamic> post(
+      {required String url,
+      @required String? body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({
+        'Authorization': 'Bearer $token',
+      });
+    }
+    http.Response response =
+        await http.post(Uri.parse(url), body: body, headers: headers);
+    Map<String , dynamic> data = jsonDecode(response.body);
+    return data;    
+  }
 }
+
+
+
+      // 'Accept': 'application/json',
+      // 'Content-type': 'application/x-www-form-urlencoded',
+      // 'Authorization': 'Bearer ',
+
+
+
+      // 'titlt' : 'test' ,
+      // 'price' : '12.5' ,
+      // 'description' : 'lorem ijsao set' ,
+      // 'image' : 'https://i.pravatar.cc' ,
+      // 'ctegory' : 'electronic' ,
--- lib/services/get_all_categories_service.dart
+++ lib/services/get_all_categories_service.dart
@@ -7,4 +7,5 @@ class AllCategoryService {
 
     return data;
   }
+  
 }
