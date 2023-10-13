5af732ad406f2d2d8c6f1ccc476f08adc39bfa7e
Hishamk99 <hishamkamel370@gmail.com>
2023-10-13 22:18:58 +0200

    product card

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -18,12 +18,59 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         actions: [
           IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.cartPlus,
-                color: Colors.black,
-              ))
+            onPressed: () {},
+            icon: const Icon(
+              FontAwesomeIcons.cartPlus,
+              color: Colors.black,
+            ),
+          )
         ],
+      ),
+      body:  Container(
+        width: 220,
+        height: 130,
+        decoration:  BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2), // تخفيف حدة اللون
+              blurRadius: 40,
+              spreadRadius: 0,
+              offset: const Offset(10,10),
+            )
+          ]
+        ),
+        child: const Card(
+          elevation: 10,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16 , vertical: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'AHandBag Lv',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      r'$225',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Icon(Icons.favorite , color: Colors.red,),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
