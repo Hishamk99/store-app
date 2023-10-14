2e144524bd4b74f61fde8530a511517a1a04cbda
Hishamk99 <hishamkamel370@gmail.com>
2023-10-14 08:55:52 +0200

    stack and positioned widget

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -26,51 +26,64 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body:  Container(
-        width: 220,
-        height: 130,
-        decoration:  BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.2), // تخفيف حدة اللون
-              blurRadius: 40,
-              spreadRadius: 0,
-              offset: const Offset(10,10),
-            )
-          ]
-        ),
-        child: const Card(
-          elevation: 10,
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16 , vertical: 16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'AHandBag Lv',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 16,
-                  ),
-                ),
-                SizedBox(height: 10,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            width: 220,
+            height: 130,
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2), // تخفيف حدة اللون
+                blurRadius: 40,
+                spreadRadius: 0,
+                offset: const Offset(10, 10),
+              )
+            ]),
+            child: const Card(
+              elevation: 10,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      r'$225',
+                      'AHandBag Lv',
                       style: TextStyle(
+                        color: Colors.grey,
                         fontSize: 16,
                       ),
                     ),
-                    Icon(Icons.favorite , color: Colors.red,),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          r'$225',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        Icon(
+                          Icons.favorite,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
+          Positioned(
+            right: 32,
+            bottom: 100,
+            child: Image.network('' , height: 85,),
+          ),
+        ],
       ),
     );
   }
